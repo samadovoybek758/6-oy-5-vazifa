eb6ea856a36bc6e06c519f2d4fe111cae3cfccd7
samadovoybek758 <oybek22samadov@gmail.com>
2024-09-22 23:23:22 +0500

    firstly

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Card from './components/Card';
+import './App.css'
+
+function App() {
+  const[input,setinput] = useState('')
+
+  const[data,setdata] = useState([])
+
+  function change_input(e) {
+    setinput(e.target.value)
+    
+  }
+
+  function add_btn(e) {
+    e.preventDefault()
+
+    const item = {
+      input: input,
+      id: Date.now()
+    }
+    let copied = [...data];
+    copied.push(item);
+    setdata(copied);
+
+    
+    let storage_copy = []
+    if (localStorage.getItem('data')) {
+      storage_copy = JSON.parse(localStorage.getItem('data'))
+     
+    }
+    storage_copy.push(item);
+    localStorage.setItem("data",JSON.stringify(storage_copy))
+
+    setinput('')
+
+  }
+
+ 
+
+  useEffect(()=>{
+    let storage_item = []
+    if (localStorage.getItem('data')) {
+      storage_item = JSON.parse(localStorage.getItem('data'))
+    }
+    setdata(storage_item)
+  },[])
+
+
+  function delet_btn(id) {
+    
+    let copy = [...data]
+    copy = copy.filter(function (value) {
+      return value.id != id
+    })
+    localStorage.setItem("data", copy)
+    setdata(copy)
+  }
+  return(
+    <>
+      <div  className="flex justify-center gap-3 mx-auto w-[600px] my-20" >
+        <input onChange={change_input} value={input} type="text" placeholder='Enter somethis...' className=' rounded-md w-[382px] bg-violet-950 px-2 text-white border-solid border-white text-base' />
+        <div onClick={add_btn} className='w-10 h-10 flex justify-center align-middle rounded-lg text-white text-2xl bg-purple-600'>+</div>
+      </div>
+      {
+        data.length > 0 && data.map(function (value,index) {
+          return <Card data={value} key={index} delet_btn={delet_btn}/>
+        })
+      }
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import delet from '../assets/delet.svg';
+import check from '../assets/Check.svg'
+
+function Card(props) {
+    const{delet_btn}= props
+  return (
+    <div className='w-[432px] flex justify-between mx-auto my-5 bg-blue-900 px-4 py-4 rounded-md align-middle ' >
+        <h3 className='text-base font-medium w-[200px] overflow-auto text-violet-500'>{props.data.input}</h3>
+        <div className='flex gap-5'>
+            <img src={check} alt="" />
+            <img src={delet} onClick={() => (delet_btn(id))} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Card
